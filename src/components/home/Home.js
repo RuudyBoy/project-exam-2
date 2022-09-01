440c81cc0bf23e467e44be881e72115f693721be
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-09-01 19:38:30 +0200

    ø

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,9 +4,11 @@ import image from '../../images/bilde.jpg';
 export default function Home() {
   return (
     <>
+    <div className='home-page'>
       <Heading title="Welcome to Holidaze" />
       <p>Visit the beatuful city of bergen</p>
       <a className='home-cta' href='/hotels'>See all hotels</a>
+    </div>
     </>
   );
 }
