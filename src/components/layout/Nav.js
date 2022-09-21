4c767fe603a159bfa8f3886a2a53e133b1f97f2a
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-09-21 23:56:00 +0200

    l

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
+import logo from "../../images/bilde.jpg" 
 //import { Navbar } from "react-bootstrap";
 //import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 //import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
@@ -19,11 +20,13 @@ function Nav() {
 		setAuth(null);
 		history.push("/");
 	}
+
 	return (
-	<Navbar  bg="dark" variant="dark" expand="lg">
+	<Navbar className="navbar" expand="lg">
+		<Link className="navbar-brand" to="/"> Holzinger</Link>
 		 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 		 <Navbar.Collapse id="basic-navbar-nav">
-		<nav className="mr-auto">
+		<nav>
 			<Link to="/">Home</Link>
             <Link to="/hotels">Hotels</Link>
             <Link to="/contact">Contact</Link>
@@ -38,10 +41,41 @@ function Nav() {
 		</nav>
 		</Navbar.Collapse>
 		</Navbar>
-
 	);
 }
 
 export default Nav;
 
 
+	/*
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+	
+*/
