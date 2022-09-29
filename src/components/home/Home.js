ff25bd3547999735b5d1340f3c0f47ac02b77680
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-09-29 19:34:18 +0200

    ø

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import Heading from '../layout/Heading';
-import { FaHotel } from 'react-icons/fa';
+import { FaHotel, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import logo from "../../images/holilogo128.png"
 import SearchHotel from './SearchHotel';
@@ -9,16 +9,18 @@ import SearchHotel from './SearchHotel';
 export default function Home() {
   return (
     <>
-    <div className="home-background">
+   
+    <div className="home-background"> 
+    <div className="dropdown-inner">
+      <SearchHotel/>
+    </div>
     <div className="home-container">
     <img className="logo" src={logo} alt="Logo" />
       <Heading title="Welcome to Holidaze!" />
       <h2>Find a hotel in the beatiful city of Bergen.</h2>
       <Link className="home-cta" to="/hotels"><FaHotel /> BOOK HOTEL</Link>
     </div> 
-    <div className="dropdown-inner">
-      <SearchHotel/>
-    </div>
+   
     </div>
     </>
   );
